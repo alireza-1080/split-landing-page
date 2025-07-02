c8553562630d8bdd03fcc3abf94faf06b3bd14f4
Alireza Aberoumand <maya7192@yahoo.com>
2025-07-01 18:21:07 -0700

    Refactor HomePage and Section components to improve interactivity and layout; added GSAP animations and state management for section selection

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,34 @@
+"use client";
 import Section from "@/components/Section";
-import ps5 from "@/public/ps5.jpg"
-import xbox from "@/public/xbox.jpg"
+import ps5 from "@/public/ps5.jpg";
+import xbox from "@/public/xbox.jpg";
+import { useState } from "react";
+import { StaticImageData } from "next/image";
+
+type SelectedSection = "PS5" | "XBOX" | "none";
 
 const HomePage = () => {
+  const [sections] = useState<
+    Array<{
+      horizontalPosition: "left" | "right";
+      image: StaticImageData;
+      alt: "PS5" | "XBOX";
+    }>
+  >([
+    { horizontalPosition: "left", image: ps5, alt: "PS5" },
+    { horizontalPosition: "right", image: xbox, alt: "XBOX" },
+  ]);
+  const [selectedSection, setSelectedSection] = useState<SelectedSection>("none");
+
+  const handlePageBlur = () => {
+    setSelectedSection("none");
+  };
+
   return (
-    <div className="relative container mx-auto h-screen w-full bg-gray-950">
-      <Section horizontalPosition="left" image={ps5} alt={"PS5"}/>
-      <Section horizontalPosition="right" image={xbox} alt={"XBOX"}/>
+    <div className="relative mx-auto h-screen w-full bg-gray-950 xl:container" onMouseLeave={handlePageBlur}>
+      {sections.map((section) => {
+        return <Section {...section} key={section.alt} setSelectedSection={setSelectedSection} selectedSection={selectedSection} />;
+      })}
     </div>
   );
 };
--- components/Section.tsx
+++ components/Section.tsx
@@ -1,33 +1,94 @@
 import Image, { StaticImageData } from "next/image";
-import React from "react";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { Dispatch, SetStateAction, useRef } from "react";
+
+gsap.registerPlugin(useGSAP);
+
+type SelectedSection = "PS5" | "XBOX" | "none";
 
 type Props = {
   image: StaticImageData;
   horizontalPosition: "left" | "right";
   alt: "PS5" | "XBOX";
+  setSelectedSection: Dispatch<SetStateAction<SelectedSection>>;
+  selectedSection: SelectedSection;
 };
 
-const Section = ({ image, horizontalPosition, alt }: Props) => {
+const Section = ({ image, horizontalPosition, alt, setSelectedSection, selectedSection }: Props) => {
+  const buttonAnimation = useRef<gsap.core.Tween>(null);
+  const button = useRef<HTMLButtonElement>(null);
+  const section = useRef<HTMLDivElement>(null);
+  const h3 = useRef<HTMLHeadingElement>(null);
+  const bgCover = useRef<HTMLDivElement>(null);
+  const tl = useRef<gsap.core.Timeline>(gsap.timeline({ paused: true }));
+
+  useGSAP(() => {
+    buttonAnimation.current = gsap.to(button.current, {
+      background: "white",
+      borderColor: "white",
+      color: "black",
+      scale: 1.1,
+      fontWeight: "700",
+      paused: true,
+    });
+  }, []);
+
+  const handleButtonHover = () => {
+    buttonAnimation.current?.play();
+  };
+
+  const handleButtonBlur = () => {
+    buttonAnimation.current?.reverse();
+  };
+
+  const handleSectionHover = () => {
+    setSelectedSection(alt);
+  };
+
+  useGSAP(() => {
+    if (selectedSection === "none") {
+      tl.current
+      .to(section.current, { width: "50%" })
+      .to(bgCover.current, { opacity: 1 }, "<")
+      .to(button.current, { opacity: 1 }, "<")
+      .to(h3.current, { rotate: "0deg", y: 0, top: "25%" }, "<")
+    } else if (selectedSection === alt) {
+      tl.current
+      .to(section.current, { width: "85%" })
+      .to(bgCover.current, { opacity: 0 }, "<")
+      .to(button.current, { opacity: 1 }, "<")
+      .to(h3.current, { rotate: "0deg", y: 0, top: 100 }, "<")
+    } else {
+      tl.current.to(section.current, { width: "15%" })
+      .to(bgCover.current, { opacity: 1 }, "<")
+      .to(button.current, { opacity: 0 }, "<")
+      .to(h3.current, { rotate: alt === "PS5" ? "-90deg" : "90deg", y: 150 }, "<")
+    }
+  }, [selectedSection]);
+
+  useGSAP(() => {
+    tl.current.kill()
+    tl.current.play();
+  }, [tl.current]);
+
   return (
-    <section
-      className={`absolute h-full w-1/2 overflow-hidden ${horizontalPosition === "left" ? "left-0 origin-left" : "right-0 origin-right"}`}
-    >
+    <section ref={section} onMouseEnter={handleSectionHover} className={`absolute h-full w-1/2 overflow-hidden ${horizontalPosition === "left" ? "left-0 origin-left" : "right-0 origin-right"}`}>
       <figure className="relative h-full w-screen">
-        <Image
-          quality={100}
-          src={image}
-          fill
-          className="h-full w-full bg-cover"
-          alt={alt}
-        />
+        <Image quality={100} src={image} fill className="h-full w-full bg-cover" alt={alt} />
       </figure>
-      <article
-        className={`absolute top-0 left-0 h-screen w-full ${alt === "PS5" ? "bg-blue-500/80" : "bg-gray-800/80"}`}
-      ></article>
-      <h3 className="absolute top-1/4 left-1/2 -translate-x-1/2 text-7xl font-bold text-nowrap">
+      <article ref={bgCover} className={`absolute top-0 left-0 h-screen w-full ${alt === "PS5" ? "bg-blue-500/80" : "bg-gray-800/80"}`}></article>
+      <h3 ref={h3} className="absolute top-1/4 left-1/2 -translate-x-1/2 translate-y-0 rotate-0 text-7xl font-bold text-nowrap">
         {alt === "PS5" ? "PlayStation 5" : "XBOX Series X"}
       </h3>
-      <button className="absolute top-2/4 uppercase text-3xl left-1/2 -translate-x-1/2 border-2 px-16 py-5">buy now</button>
+      <button
+        ref={button}
+        className="absolute top-2/4 left-1/2 -translate-x-1/2 cursor-pointer border-4 px-16 py-5 text-3xl text-nowrap uppercase"
+        onMouseEnter={handleButtonHover}
+        onMouseLeave={handleButtonBlur}
+      >
+        buy now
+      </button>
     </section>
   );
 };
